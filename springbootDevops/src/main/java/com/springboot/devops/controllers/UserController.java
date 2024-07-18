c89d9e1878e853e6d392f3633609fbbc3a35cf98
garava <garava@altimetrik.com>
2024-07-18 23:27:05 +0530

    feature/api-impl api implementation integrate to aws rds

--- springbootDevops/src/main/java/com/springboot/devops/controllers/UserController.java
+++ springbootDevops/src/main/java/com/springboot/devops/controllers/UserController.java
@@ -1,20 +1,47 @@
 package com.springboot.devops.controllers;
 
+import com.springboot.devops.entity.User;
+import com.springboot.devops.repository.UserRepository;
+import com.springboot.devops.service.UserService;
 import com.springboot.devops.utils.Utility;
-import org.apache.catalina.mbeans.UserMBean;
-//import org.springframework.security.core.userdetails.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(Utility.base_url)
 public class UserController {
 
+    @Autowired
+    private  UserService userService;
+
+    @GetMapping("/getAllUsers")
+    public ResponseEntity<List<User>> getAllUsers() {
+
+        List<User> userlist = userService.getUsersList();
+        if (userlist != null) {
+            return ResponseEntity.ok(userlist);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
+    }
 
     @GetMapping("/user")
-    public String getUserById(@RequestParam("userId") Long userId) {
-        return "welcome to spring boot";
+    public ResponseEntity<User> getById(@RequestParam("Id") Integer Id) {
+
+        User user = userService.getUserById(Id);
+        if (user != null) {
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/user")
+    public User saveUser(@RequestBody User user) {
+        return userService.save(user);
     }
 }
